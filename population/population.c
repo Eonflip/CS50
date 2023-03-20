ce33340ce2543864400b4599ffcbf09c71f92cb0
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-03-20 19:55:14 +0000

    Mon, Mar 20, 2023, 7:55 PM +00:00

--- population/population.c
+++ population/population.c
@@ -4,11 +4,24 @@
 int main(void)
 {
     // TODO: Prompt for start size
-    int start_size = 
+    int start_size;
+    int end_size;
+    do
+    {
+        start_size = get_int("Start Size\n");
+    }
+    while (start_size < 1);
 
     // TODO: Prompt for end size
+    do
+    {
+        end_size = get_int("End Size\n");
+    }
+    while (end_size < 1);
 
     // TODO: Calculate number of years until we reach threshold
+    
+
 
     // TODO: Print number of years
 }
