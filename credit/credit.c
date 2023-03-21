150885b4aa9fcdcb43aa12ea35ffcad17ad9b83d
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-03-21 20:13:25 +0000

    Tue, Mar 21, 2023, 2:13 PM -06:00

--- credit/credit.c
+++ credit/credit.c
@@ -12,6 +12,10 @@ int main(void)
 
     while (card_number < 0 || card_number > 5000000000000000);
 
+    int first_digit = card_number;
+
+    
+
     if (card_number[0] == 4 && Math.floor(Math.log10(Math.abs(card_number))) == 13 || 16){
         printf("VISA");
     }
