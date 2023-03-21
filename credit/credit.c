c692057de89b14e42ae494909511868377d1b2c9
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-03-21 21:06:17 +0000

    Tue, Mar 21, 2023, 3:06 PM -06:00

--- credit/credit.c
+++ credit/credit.c
@@ -15,16 +15,46 @@ int main(void)
     while (card_number < 0 || card_number > 5000000000000000);
 
     long first_digit = card_number;
+    long two_digits = card_number;
 
     while (first_digit >= 10)
     {
         first_digit = first_digit / 10;
     }
 
-    //print function to test if the first digit is being logged
-    //printf("%ld\n", first_digit);
+    while (two_digits >= 100)
+    {
+        two_digits = two_digits / 10;
+    }
 
+    //print function to test if the first digit is being logged
+    printf("%ld\n", two_digits);
+
+    //Test if Visa
     if (first_digit == 4)
+    {
+        if (floor(log10(labs(card_number))) + 1 == 13)
+        {
+            printf("VISA\n");
+        }
+        else if (floor(log10(labs(card_number))) + 1 == 16)
+        {
+            printf("VISA\n");
+        }
+    }
+
+    //Test if Amex
+    if (first_digit == 34 || first_digit == 37)
+    {
+        if (floor(log10(labs(card_number))) + 1 == 15)
+        {
+            printf("AMEX\n");
+        }
+    }
+
+
+    //Test if MasterCard
+    if (first_digit == 51 || first_digit == 52 || first_digit == 53 || first_digit == 54 || first_digit == 55)
     {
         if (floor(log10(labs(card_number))) + 1 == 13)
         {
