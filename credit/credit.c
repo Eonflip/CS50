9245eca10578fa0f1c3718999d029a2decce281b
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-03-21 20:23:00 +0000

    Tue, Mar 21, 2023, 2:23 PM -06:00

--- credit/credit.c
+++ credit/credit.c
@@ -12,14 +12,14 @@ int main(void)
 
     while (card_number < 0 || card_number > 5000000000000000);
 
-    int first_digit = card_number;
+    long first_digit = card_number;
 
-    while (first_digit > 10)
+    while (first_digit >= 10)
     {
-        first_digit /= 10;
+        first_digit = first_digit / 10;
     }
 
-    printf("%i\n", first_digit);
+    printf("%ld\n", first_digit);
 
     //if (card_number[0] == 4 && Math.floor(Math.log10(Math.abs(card_number))) == 13 || 16)
     //{
