d614d4067e25be5dba4eab177434e2d3e837c9a6
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-03-21 20:03:01 +0000

    Tue, Mar 21, 2023, 2:03 PM -06:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,7 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
+    long card_number;
+
+    do
+    {
+        card_number = get_long("Please enter your credit card number: \n");
+    }
+
+    while (card_number < 0 || card_number > 5000000000000000)
+
     
 }
