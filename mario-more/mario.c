a79797470f369b60af1eb00dbb7124fddf5962bd
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-03-21 02:08:40 +0000

    Mon, Mar 20, 2023, 8:08 PM -06:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -4,15 +4,15 @@
 int main(void)
 {
     int height;
-    int column;
-    int linenum;
 
+    //do-while loop to make sure that the only number accepted is 1-8 inclusive
     do
     {
         height = get_int("Height: ");
     }
     while (height < 1 || height > 8);
 
+    //for loop to loop through each column for height entered
     for (int i = 0; i < height; i++)
     {
 
