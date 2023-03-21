db0d1bc0add692ea3b7350cb0ea130bda4fef38a
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-03-21 01:57:58 +0000

    Mon, Mar 20, 2023, 7:57 PM -06:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -11,7 +11,7 @@ int main(void)
     {
        height = get_int("Height: ");
     }
-    while (height < 1 && height > 8);
+    while (height < 1 || height > 8);
 
     for (int i = 0; i < height; i++) {
 
