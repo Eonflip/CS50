11a83945c2140d61f718ff6c9940c7f317459c7f
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-03-22 16:25:03 +0000

    Wed, Mar 22, 2023, 10:25 AM -06:00

--- credit/credit.c
+++ credit/credit.c
@@ -90,5 +90,13 @@ int main(void)
 
 int checksum(void)
 {
-    
+    int first = card_number % 10000000000000000
+    int third = card_number % 100
+    int fifth = card_number % 1000000000000
+    int seventh = card_number % 10000000000
+    int ninth = card_number % 100000000
+    int eleventh = card_number % 1000000
+    int thirteenth = card_number % 10000
+    int fifteenth = card_number % 100
+
 }
