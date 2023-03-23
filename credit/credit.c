02595a013db0ff4e70bb654b9f45da73fdb68fef
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-03-23 21:51:03 +0000

    Thu, Mar 23, 2023, 3:51 PM -06:00

--- credit/credit.c
+++ credit/credit.c
@@ -14,7 +14,19 @@ int main(void)
 int validate(long n)
 {
     long number = n;
+    int sum = 0;
 
     printf("Number: %ld\n", number);
-    
+
+
+    sum = number % 10;
+    number = number / 10;
+    while(number)
+    {
+        int temp = (number % 10) * 2;
+        if (temp > 9)
+        {
+            temp = temp - 9
+        }
+    }
 }
