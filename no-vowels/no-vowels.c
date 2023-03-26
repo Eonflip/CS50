83718e58eb8c8c2d58dfc5da01c021a7c7d7532c
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-03-26 19:41:48 +0000

    Sun, Mar 26, 2023, 1:41 PM -06:00

--- no-vowels/no-vowels.c
+++ no-vowels/no-vowels.c
@@ -22,7 +22,7 @@ int main(int argc, string argv[])
     }
 }
 
-string replace (string argv[])
+int replace (string argv[])
 {
     string input = argv[1];
     for (int i = 0; i < strlen(input); i++)
