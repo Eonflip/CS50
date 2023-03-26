8b0c951966d30494b32c74ae513a12730dbe7fa2
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-03-26 22:04:04 +0000

    Sun, Mar 26, 2023, 4:04 PM -06:00

--- no-vowels/no-vowels.c
+++ no-vowels/no-vowels.c
@@ -7,11 +7,10 @@
 #include <stdio.h>
 #include <string.h>
 
-string replace(string argv[]);
+string replace(argv[]);
 
 int main(int argc, string argv[])
 {
-    string output = replace (argv[1]);
     if (argc == 2)
     {
         printf("%s\n", output);
@@ -24,28 +23,7 @@ int main(int argc, string argv[])
     }
 }
 
-string replace (string argv[])
+string replace (argv[])
 {
-    string input = argv[0];
-    for (int i = 0; i < strlen(input); i++)
-    {
-        if (input[i] == 'a' || input[i] == 'A')
-        {
-            input[i] = '6';
-        }
-        else if (input[i] == 'e' || input[i] == 'E')
-        {
-            input[i] = '3';
-        }
-        else if (input[i] == 'i' || input[i] == 'I')
-        {
-            input[i] = '1';
-        }
-        else if (input[i] == 'o' || input[i] == 'O')
-        {
-            input[i] = '0';
-        }
 
-    }
-return input;
 }
