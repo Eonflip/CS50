32d0b320c5e458ddb201eed7092c53dccace79bc
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-03-28 18:09:17 +0000

    Tue, Mar 28, 2023, 12:09 PM -06:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,15 +1,30 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int count_letters(string sentence);
 
 int main(void)
 {
-    string test_sentence = get_string("Text: ")
+    string test_sentence = get_string("Text: ");
     printf("\n");
     printf("%s\n", test_sentence);
+
+    count_letters(test_sentence);
 }
 
 
 int count_letters(string sentence)
 {
-    string sentence = 
+    int num_char = 0;
+
+    for (int i = 0; i < strlen(sentence); i++)
+    {
+        if (isalphanum(sentence[i]))
+        {
+            printf(sentence[i]);
+            num_char += 1;
+        }
+    }
 }
