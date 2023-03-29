3e8d84762c87ca311345ae62ddfb01bc2b43489d
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-03-29 22:16:03 +0000

    Wed, Mar 29, 2023, 4:16 PM -06:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -3,13 +3,18 @@
 
 int main(int argc, string argv[])
 {
+    do
+    {
+        printf
+    }
+
     string cipher_key[26] = {}
 
     for (int i = 0; i < 26; i++)
     {
         cipher_key[i] = argv[1][i]
     }
-    
+
     if (argc == 2)
     {
         printf(")
