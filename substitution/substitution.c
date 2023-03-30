508367976d265a8d0908bec50d9663d73b5898f2
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-03-30 01:13:37 +0000

    Wed, Mar 29, 2023, 7:13 PM -06:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -9,23 +9,13 @@
 int main(int argc, string argv[])
 {
 
-    string cipher_key[26] = {};
-
     if (argc == 2)
     {
-        for (int i = 0; i < 26; i++)
-        {
-            do
-            {
-                printf("Key must only contain 26 unique alphanumeric characters");
-            }
-            while (isalnum(argv[1][i]) != true);
-
-            cipher_key[i] = argv[1][i];
-        }
+        
     }
     else
     {
         printf("Please enter a single argument that is 26 unique alphanumeric characters");
     }
+
 }
