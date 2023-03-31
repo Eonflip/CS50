92c2aa9032e7c69f90b05ecd9a50c53eff566e64
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-03-31 22:22:39 +0000

    Fri, Mar 31, 2023, 4:22 PM -06:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -3,86 +3,79 @@
 #include <ctype.h>
 #include <string.h>
 #include <math.h>
-#include <limits.h>
-#include <stdlib.h>
+#include <stdbool.h>
+
+bool checkUnique(char str[]);
 
 int main(int argc, string argv[])
 {
+    int key_length = strlen(argv[1]);
+    char cipher[26];
+
     if (argc != 2)
     {
-        printf("Usage: ./substitution key(26 unique letters)\n");
+        printf("Usage: ./substitution key (26 unique letters) \n");
         return 1;
     }
-
-    int key_length = strlen(argv[1]);
-
-    if (key_length == 26)
+    else if (key_length != 26)
     {
-        char Seen[UCHAR_MAX + 1] = {0};
-        for (int i = 0; i < 26; i++)
-        {
-            unsigned char c = argv[1][i];
-            c = toupper(c);
-            if (Seen[c])
-            {
-                printf("Key must contain 26 unique non-repeating alphanumeric characters\n");
-                return 1;
-            }
-            Seen[c] = 1;
-        }
-    }
-
-    if (key_length != 26)
-    {
-        printf("Key must contain 26 alphanumeric characters.\n");
+        printf("Key must contain 26 unique alphanumeric characters\n");
         return 1;
     }
-
-    char cipher[26];
-    for (int i = 0; i < 26; i++)
+    else if (key_length == 26)
     {
-        if (!isalnum(argv[1][i]))
+        if (checkUnique(argv[1]))
         {
-            printf("Key must only contain alphanumeric characters.\n");
-            return 1;
-        }
-
-        for (int j = 0; j < i; j++)
-        {
-            if (toupper(argv[1][i]) == toupper(cipher[j]))
+            for(int i = 0; i < 26; i++)
             {
-                printf("Key must not contain repeated characters.\n");
-                return 1;
+                if (!isalnum(argv[1][i]))
+                {
+                    printf("Key must only contain alphanumeric characters.\n");
+                    return 1;
+                }
+                else
+                {
+                    string plaintext = get_string("plaintext: ");
+                    char key[] = argv[1];
+                    string cipher_key = key;
+                    char ciphertext[strlen(plaintext)];
+
+                    for (int i = 0; i < strlen(plaintext); i++) {
+                        if (plaintext[i] == ' ')
+                        {
+                            ciphertext[i] = ' ';
+                        }
+                        else
+                        {
+                            int index = plaintext[i] - 'A';
+
+                            ciphertext[i] = key[index];
+                        }
+                    }
+                    printf("Plaintext: %s\n", plaintext);
+                    printf("Ciphertext: %s\n", ciphertext);
+                    return 0;
+                }
             }
         }
-
-        cipher[i] = toupper(argv[1][i]);
     }
+}
 
-    // do something with the cipher
-    string plaintext = get_string("plaintext: ");
-    string cipher_text = malloc(strlen(plaintext) + 1);
-    for (int i = 0, n = strlen(plaintext); i < n; i++)
+
+bool checkUnique(char str[])
+{
+    bool char_set[256] = { false };
+
+    for (int i = 0; str[i] != '\0'; i++)
     {
-        char c = plaintext[i];
-        if (isalpha(c))
-        {
-            // convert character to uppercase and subtract ASCII value of 'A' to get index
-            int index = toupper(c) - 'A';
-            // use index to look up corresponding character in cipher array
-            char encrypted_char = cipher[index];
-            // copy the encrypted character to the cipher_text array
-            cipher_text[i] = encrypted_char;
+        int val = str[i];
+        if (char_set[val]) {
+            return false;
         }
         else
         {
-            // print non-alphabetic character unchanged
-            cipher_text[i] = c;
+            char_set[val] = true;
         }
-
     }
-    cipher_text[strlen(plaintext)] = '\0';
-    printf("%s", cipher_text);
-    free(cipher_text);
-    return 0;
+    return true;
 }
