50104608a759717cb039afa8d7db8bd9ec74cd7d
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-04-07 22:22:43 +0000

    Fri, Apr 7, 2023, 4:22 PM -06:00

--- temps/temps.c
+++ temps/temps.c
@@ -62,6 +62,7 @@ int main(void)
 // TODO: Sort cities by temperature in descending order
 void sort_cities(void)
 {
+    int counter = 0;
     int lowest_num = temps[0].temp;
     // Add your code here
     for (int i = 0; i < NUM_CITIES; i++)
