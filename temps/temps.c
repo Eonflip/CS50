65334a906c41c8903988d0166b98a8552300f469
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-04-07 22:30:51 +0000

    Fri, Apr 7, 2023, 4:30 PM -06:00

--- temps/temps.c
+++ temps/temps.c
@@ -63,11 +63,13 @@ int main(void)
 void sort_cities(void)
 {
     int counter = 0;
-    
     // Add your code here
     for (int i = 0; i < NUM_CITIES; i++)
     {
-        if ()
+        if (temps[i].temp < temps[counter].temp)
+        {
+            
+        }
     }
 
 }
