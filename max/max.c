e532784a0af9bd8d5f3388b6120858a14002b1ca
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-04-10 22:01:31 +0000

    Mon, Apr 10, 2023, 4:01 PM -06:00

--- max/max.c
+++ max/max.c
@@ -30,7 +30,7 @@ int max(int array[], int n)
 
     for (int i = 0; i < n - 1; i++)
     {
-        int max_num = array[0];
+        int max_num = array[i];
         for (int j = i + 1; j < n; j++)
         {
             if (array[j] > array[i])
