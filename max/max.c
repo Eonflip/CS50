2e3c9505c86eb7f67a57047c8bf6488bbff39609
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-04-10 22:12:34 +0000

    Mon, Apr 10, 2023, 4:12 PM -06:00

--- max/max.c
+++ max/max.c
@@ -27,7 +27,10 @@ int main(void)
 // TODO: return the max value
 int max(int array[], int n)
 {
+    //initialize the max_num as the first element in the passed array (this may or may not change later)
     int max_num = array[0];
+
+    //write a selection sort loop without changing the position of the array element once it's found to not be in the right place
     for (int i = 0; i < n - 1; i++)
     {
         for (int j = i + 1; j < n; j++)
