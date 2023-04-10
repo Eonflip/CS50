9cfd262e9b98b91f9b8113dd798802b677163851
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-04-10 21:44:18 +0000

    Mon, Apr 10, 2023, 3:44 PM -06:00

--- max/max.c
+++ max/max.c
@@ -11,7 +11,7 @@ int main(void)
     do
     {
         n = get_int("Number of elements: ");
-    } 
+    }
     while (n < 1);
 
     int arr[n];
@@ -27,5 +27,10 @@ int main(void)
 // TODO: return the max value
 int max(int array[], int n)
 {
+    int max_num = 1;
+    for (int i = 0; i < n - 1; i++)
+    {
+        
+    }
     return 0;
 }
