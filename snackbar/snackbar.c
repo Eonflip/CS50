6231278cec6989facf6cacd7776ffc830264e158
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-04-11 02:21:58 +0000

    Mon, Apr 10, 2023, 8:21 PM -06:00

--- snackbar/snackbar.c
+++ snackbar/snackbar.c
@@ -75,25 +75,44 @@ int main(void)
 // the line above should read "four"
 void add_items(void)
 {
-    menu[0].item = "Burger"
-    menu[0].price = 9.5
+    menu[0].item = "Burger";
+    menu[0].price = 9.5;
+
+    menu[1].item = "Vegan Burger";
+    menu[1].price = 11;
+
+    menu[2].item = "Hot Dog";
+    menu[2].price = 5;
+
+    menu[3].item = "Cheese Dog";
+    menu[3].price = 7;
+
+    menu[4].item = "Fries";
+    menu[4].price = 5;
+
+    menu[5].item = "Cheese Fries";
+    menu[5].price = 6;
+
+    menu[6].item = "Cold Pressed Juice";
+    menu[6].price = 7;
+
+    menu[7].item = "Cold Brew";
+    menu[7].price = 3;
+
+    menu[8].item = "Water";
+    menu[8].price = 2;
+
+    menu[9].item = "Soda";
+    menu[9].price = 2;
+
+
     return;
 }
 
 // Search through the menu array to find an item's cost
 float get_cost(string item)
 {
+
     return 0.0;
 }
 
- * Beach Burger Shack has the following 10 items on their menu
- * Burger: $9.5
- * Vegan Burger: $11
- * Hot Dog: $5
- * Cheese Dog: $7
- * Fries: $5
- * Cheese Fries: $6
- * Cold Pressed Juice: $7
- * Cold Brew: $3
- * Water: $2
- * Soda: $2
