35b26cc3d4dbcafc48b59f18bf9526ada71c8efe
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-05-03 02:26:54 +0000

    Tue, May 2, 2023, 8:26 PM -06:00

--- volume/volume.c
+++ volume/volume.c
@@ -37,7 +37,7 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
-    fread(output, 44, 1, input);
+    fread(&output, 44, 1, input);
 
     // TODO: Read samples from input file and write updated data to output file
 
