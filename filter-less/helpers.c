810a1aba0afee454b49a03e9d9417c5ccf0f80c8
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-06-05 19:27:42 +0000

    Mon, Jun 5, 2023, 1:27 PM -06:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,12 +3,34 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            RGBTRIPLE pixel = image[i][j];
+
+            char average = (pixel.rgbtRed + pixel.rgbtBlue + pixel.rgbtGreen) / 3.0;
+
+            image[i][j].rgbtBlue = average;
+            image[i][j].rgbtRed = average;
+            image[i][j].rgbtGreen = average;
+        }
+    }
     return;
 }
 
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            RGBTRIPLE pixel = image[i][j];
+
+            char sepiaRed = 
+        }
+    }
     return;
 }
 
