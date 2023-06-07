2662264d6419f497fcdba5ac0819d9add01e8071
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-06-07 02:47:54 +0000

    Tue, Jun 6, 2023, 8:47 PM -06:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -70,5 +70,6 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     }
 
     int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+    int dy[] = {-1, 0, 1, -1, 1, 1, -1, 0, 1};
     return;
 }
