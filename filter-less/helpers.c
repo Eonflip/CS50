1971e729b680bf9a517cde0d3b9289af6f18580a
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-06-07 02:45:41 +0000

    Tue, Jun 6, 2023, 8:45 PM -06:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -60,5 +60,13 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE original[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            original[i][j] = image[i][j];
+        }
+    }
     return;
 }
