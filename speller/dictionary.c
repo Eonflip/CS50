391db6ef59e1c98bf6af2f03acc47450e78fb1cb
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-06-24 01:51:54 +0000

    Fri, Jun 23, 2023, 7:51 PM -06:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -39,20 +39,41 @@ unsigned int hash(const char *word)
     int hash_value = 0;
     int len = strlen(word);
 
-    if (len <= 1 && isalpha(word[0]))
+    // Check for first character (assuming it exists)
+    if (len >= 1 && isalpha(word[0]))
     {
         hash_value += 677 * (tolower(word[0]) - 'a');
     }
-    if (len >= 2 && isalpha(word[1]))
+
+    if (len >= 2)
     {
-        hash_value += 27 * (tolower(word[1]) - 'a');
-    }
-    if (len >= 3 && isalpha(word[2]))
-    {
-        hash_value += tolower(word[2]) - 'a';
+        if (isalpha(word[1]))
+        {
+            hash_value += 27 * (tolower(word[1]) - 'a');
+        }
+        else
+        {
+            // Handle the case where second character is not a letter (e.g. an apostrophe)
+            hash_value += 1;
+            return hash_value;
+        }
     }
 
-    return hash_value;
+    // Check for third character or apostrophe
+    if (len >= 3)
+    {
+        if (isalpha(word[2]))
+        {
+            hash_value += tolower(word[2]) - 'a';
+        }
+        else
+        {
+            // Handle the case where third character is not a letter
+            hash_value += 1;
+            return hash_value;
+        }
+    }
+    return hash_value
 }
 
 // Loads dictionary into memory, returning true if successful, else false
