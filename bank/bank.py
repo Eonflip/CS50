43de195d0f261b6b840ec092c20f5d8bf113ce56
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-06-26 13:45:20 +0000

    Mon, Jun 26, 2023, 7:45 AM -06:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,12 +1,12 @@
 from cs50 import get_string
 
-greeting = get_string("Greeting:")
+greeting = get_string("Greeting: ")
 
 greeting = greeting.lower()
 
 if greeting.find("hello") != -1:
     print("$0")
-elif greeting[1] == "h":
+elif greeting[0] == "h":
     print("$20")
 else:
     print("$100")
