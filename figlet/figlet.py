d0c960b055d614def75c5f038e4ee46c729af582
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-06-26 18:36:45 +0000

    Mon, Jun 26, 2023, 12:36 PM -06:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -3,13 +3,17 @@ from sys import argv
 from cs50 import get_string
 
 figlet = Figlet()
+fontList = figlet.getFonts();
 
 for i in range(len(argv)):
     print(argv[i])
 
-if len(argv) == 3:
-    input = get_string("Input: ")
-    f = Figlet(font = argv[1])
-    print(f.renderText(f))
+for i in range(len(fontList)):
+    print(fontList[i])
+
+#if len(argv) == 3:
+#    input = get_string("Input: ")
+#    f = Figlet(font = argv[1])
+#    print(f.renderText(f))
 #elif len(argv) == 1
 
