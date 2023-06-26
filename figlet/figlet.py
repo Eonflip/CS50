ed3548e9aa5bb5618ed10e0df2554308b3b5d484
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-06-26 18:29:41 +0000

    Mon, Jun 26, 2023, 12:29 PM -06:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -2,9 +2,10 @@ from pyfiglet import Figlet
 from sys import argv
 from cs50 import get_string
 
-if len(argv) == 2
-    input = get_string("Input: ")
-    f = Figlet(font = argv[2])
-    print(f.renderText(f))
+for 
 
-
+#if len(argv) == 2
+#    input = get_string("Input: ")
+#    f = Figlet(font = argv[1])
+#    print(f.renderText(f))
+#elif len(argv) == 0
