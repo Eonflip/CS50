f84414581cb3f991abdbac1cf57f1cc995a66c42
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-06-26 13:43:45 +0000

    Mon, Jun 26, 2023, 7:43 AM -06:00

--- bank/bank.py
+++ bank/bank.py
@@ -4,9 +4,9 @@ greeting = get_string("Greeting:")
 
 greeting = greeting.lower()
 
-if greeting[0] == "h":
-    print("$20")
-elif greeting.find("hello") != -1:
+if greeting.find("hello") != -1:
     print("$0")
+elif greeting[1] == "h":
+    print("$20")
 else:
     print("$100")
