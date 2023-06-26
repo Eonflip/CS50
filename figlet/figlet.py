a08e9cfad69c5c1bc981825a55b4afce75a244c7
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-06-26 18:25:05 +0000

    Mon, Jun 26, 2023, 12:25 PM -06:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,5 +1,10 @@
 from pyfiglet import Figlet
 from sys import argv
+from cs50 import get_string
 
 if len(argv) == 2
-    
+    figlet.setFont(font = argv[2])
+    input = get_string("Input: ")
+    print(figlet.renderText(input))
+
+
