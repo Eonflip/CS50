61fa1a522f56401bf6560d2a51ad39834f0e6a19
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-06-29 20:07:38 +0000

    Thu, Jun 29, 2023, 2:07 PM -06:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,3 +1,11 @@
 # TODO
+import cs50
+
 def main():
-    
+
+    while True:
+        try:
+            card_number = input("Please enter a valid credit card number: ")
+            
+        except ValueError:
+            print("That is not a valid credit card number\n")
