3462db58d2836f946d2c92d84b2481b020d401d1
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-06-29 18:25:11 +0000

    Thu, Jun 29, 2023, 12:25 PM -06:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,20 +1,19 @@
 # TODO
-
+# create main function
 def main():
+    # continue to ask for user input until they enter a number between 1-8
     while True:
         try:
             user_input = int(input("Please enter a number: "))
 
-            if user_input > 0  and user_input <= 8:
+            if user_input > 0 and user_input <= 8:
                 break
-
+        # if user does not enter an int throw value error
         except ValueError:
             print("You must enter a number...")
-
+    # loop through numbers in range to print # tower
     for num in range(1, user_input + 1):
         print((" " * (user_input - num)) + ("#" * num) + (" " * 2) + ("#" * num))
-
-
 
 
 main()
