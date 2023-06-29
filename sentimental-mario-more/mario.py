86f91786475d965f7919e61e7f8a90f438e39a40
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-06-29 18:13:57 +0000

    Thu, Jun 29, 2023, 12:13 PM -06:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -4,7 +4,7 @@ def main():
     while True:
         user_input = int(input("Please enter a number: "))
 
-        if user_input > 0  and user_input <= 8:
+        if user_input > 0  and user_input <= 8 and user_input is not str:
             break
 
     for num in range(1, user_input + 1):
