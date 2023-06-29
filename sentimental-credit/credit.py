b69d62a2bd4a1f97215858224d8d4eb193acb679
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-06-29 20:36:05 +0000

    Thu, Jun 29, 2023, 2:36 PM -06:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,13 +1,10 @@
-# TODO
 import cs50
 
 def validate(n):
-
     number = n
     sum = 0
-
     sum = number % 10
-    number = number / 10
+    number = number // 10  # use integer division
 
     while number:
         temp = (number % 10) * 2
@@ -15,47 +12,42 @@ def validate(n):
             temp = temp - 9
 
         sum += temp
-        number /= 10
+        number //= 10
         temp = number % 10
         sum += temp
-        number /= 10
-
+        number //= 10
 
     while n > 100:
-        n = n / 10
-
+        n = n // 10
 
     if sum % 10:
-        break
-
+        return False
 
     return n
 
-
 def get_cc():
-
     while True:
         try:
             card_number = int(input("Please enter a valid credit card number: "))
-            if card_number > 0 and card_number < 50000000000000000:
-                if len(card_number) == 13 or len(card_number) == 15 or len(card_number) == 16:
+            if card_number > 0:
+                if len(str(card_number)) in [13, 15, 16]:  # convert to string before len()
                     return card_number
         except ValueError:
-            break
+            print("Invalid input. Please enter an integer.")
 
-
-def switch(validate(get_cc())):
-    if 34 or 37:
+def switch(n):
+    if n == 34 or n == 37:
         return "AMEX\n"
-    elif num in range(40, 50):
+    elif 40 <= n < 50:
         return "VISA\n"
-    elif num in range(51, 56):
+    elif 51 <= n < 56:
         return "MASTERCARD\n"
     else:
         return "INVALID\n"
 
-
-switch()
+card_number = get_cc()
+valid_number = validate(card_number)
+switch(valid_number)
 
 
 
