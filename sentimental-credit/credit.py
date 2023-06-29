ed6e03369ab028d3dcb27dad2bb6a6c93acdb500
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-06-29 20:15:44 +0000

    Thu, Jun 29, 2023, 2:15 PM -06:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -3,13 +3,7 @@ import cs50
 
 def main():
 
-    while True:
-        try:
-            card_number = int(input("Please enter a valid credit card number: "))
-            for num in card_number:
-
-        except ValueError:
-            print("That is not a valid credit card number\n")
+    switch(validate(get_cc()))
 
 
 
@@ -19,8 +13,7 @@ def get_cc():
         try:
             card_number = int(input("Please enter a valid credit card number: "))
         except ValueError:
-            print("That is not a valid credit card number\n")
-            return 0
+            return -1
 
 
 def validate():
