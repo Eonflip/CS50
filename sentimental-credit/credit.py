dca3cece6f12e10060c1e835590c5b33f9597f07
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-06-29 21:00:30 +0000

    Thu, Jun 29, 2023, 3:00 PM -06:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,13 +1,53 @@
-import cs50
+import math
+
+def get_cc():
+    n = int(input("Enter a credit card number: "))
+    if 0 <= n <= 50000000000000000:
+        num_digits = int(math.log10(abs(n))) + 1
+        if num_digits in {13, 15, 16}:
+            return n
+    return -1
+
+def validate(n):
+    sum_digits, number = n % 10, n // 10
+
+    while number > 0:
+        temp = (number % 10) * 2
+        temp = temp - 9 if temp > 9 else temp
+        sum_digits += temp + number // 10 % 10
+        number //= 100
+
+    while n > 100:
+        n //= 10
+
+    return -1 if sum_digits % 10 else n
 
 def main():
-    while True:
-        try:
-            card_number = get_int("Please enter a valid card number: ")
-            if card_number > 0 and card_number < 50000000000000000:
-                if 
-        except ValueError:
-            print("That is not a number")
+    card_types = {
+        34: "AMEX",
+        37: "AMEX",
+        40: "VISA",
+        41: "VISA",
+        42: "VISA",
+        43: "VISA",
+        44: "VISA",
+        45: "VISA",
+        46: "VISA",
+        47: "VISA",
+        48: "VISA",
+        49: "VISA",
+        51: "MASTERCARD",
+        52: "MASTERCARD",
+        53: "MASTERCARD",
+        54: "MASTERCARD",
+        55: "MASTERCARD",
+    }
+
+    print(card_types.get(validate(get_cc()), "INVALID"))
+
+if __name__ == "__main__":
+    main()
+
 
 
 
