2e1e8f4eef02da73e9879786f444c9093e8d21ca
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-06-29 20:55:55 +0000

    Thu, Jun 29, 2023, 2:55 PM -06:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -2,9 +2,15 @@ import cs50
 
 def main():
     while True:
-        card_number = get_int("Please enter a valid card number: ")
-            if card_number > 0 and card_number < 50000000000000000:
-                
+        try:
+            card_number = get_int("Please enter a valid card number: ")
+        except ValueError:
+            print("That is not a number")
+
+
+    
+
+
 
 
 
