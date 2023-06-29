72ba2b407fa19b571d21954375bc8b7866b30ea1
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-06-29 20:19:23 +0000

    Thu, Jun 29, 2023, 2:19 PM -06:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -11,7 +11,9 @@ def get_cc():
     while True:
         try:
             card_number = int(input("Please enter a valid credit card number: "))
-            if card_number 
+            if card_number > 0 and card_number < 50000000000000000:
+                if len(card_number) == 13 or len(card_number) == 15 or len(card_number) == 16:
+                    return card_number
         except ValueError:
             return -1
 
