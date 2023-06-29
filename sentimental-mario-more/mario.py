582c53fb212ae80569b90aa5bcd97cb0889e5432
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-06-29 17:48:42 +0000

    Thu, Jun 29, 2023, 11:48 AM -06:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,1 +1,11 @@
 # TODO
+
+def main():
+    while True:
+        user_input = int(input("Please enter a number: "))
+
+        if user_input > 0  and user_input <= 8:
+            break
+
+    for num in range(0:user_input)
+        print(" ")
