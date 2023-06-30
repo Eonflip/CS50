17e5195cbc096fec6b876ddc840f6ac7ca4c1a63
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-06-30 21:41:14 +0000

    Fri, Jun 30, 2023, 3:41 PM -06:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -5,5 +5,25 @@ def main():
     sentence = input("Text: ")
     print(sentence)
 
-    l = float(len(sentence) / float())
+    totalWords = 0
+    totalSentences = 0
+
+    for letter in sentence:
+        if letter = " ":
+            totalWords += 1
+        if letter = "!" or letter = "?" or letter = ".":
+            totalSentences += 1
+
+    l = float(len(sentence) / totalWords) * 100
+
+    s = float(totalSentences / totalWords) * 100
+
+    grade_level = round(0.0588 * l - 0.296 * s - 15.8)
+
+    if grade_level >= 1 and grade_level < 16:
+        print(f"Grade {grade_level}")
+    elif grade_level >= 16:
+        print("Grade 16+")
+    else:
+        print(Before Grade 1)
 
