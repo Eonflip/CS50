b6c1a025c8c6a5331d9b1018a3aa40ff41cc01d1
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-06-30 22:03:11 +0000

    Fri, Jun 30, 2023, 4:03 PM -06:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -16,12 +16,13 @@ def main():
             totalSentences += 1
 
     totalChar = len(excerpt) - (totalWords - 1) - totalSentences
+    print(len(excerpt))
 
     l = (totalChar / totalWords) * 100
 
     s = (totalSentences / totalWords) * 100
 
-    grade_level = math.round(0.0588 * l - 0.296 * s - 15.8)
+    grade_level = round(0.0588 * l - 0.296 * s - 15.8)
 
     if grade_level >= 1 and grade_level < 16:
         print(f"Grade {grade_level}")
