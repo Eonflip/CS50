f26005d4244770bad34c3a3fa6250b25700d341d
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-07-04 21:07:57 +0000

    Tue, Jul 4, 2023, 3:07 PM -06:00

--- dna/dna.py
+++ dna/dna.py
@@ -14,18 +14,33 @@ def main():
         with open(sys.argv[1]) as e:
             reader = csv.DictReader(e)
             database = list(reader)
-            print(database)
-            print(reader)
 
     # TODO: Read DNA sequence file into a variable
     if len(sys.argv) == 3:
-        dnaFile = open(sys.argv[2], "r")
-        dnaDictFile = csv.reader(dnaFile)
+        with open(sys.argv[2]) as f:
+            sequence = f.read()
 
     # TODO: Find longest match of each STR in DNA sequence
+    matches = {}
 
+    for i in database[0]:
+        matches[i] = (longest_match(sequence, i))
 
     # TODO: Check database for matching profiles
+    suspect = 'No Match'
+    suspect_counter = 1
+
+    for i in range(len(database)):
+        for j in matches:
+            if str(matches[j]) == database[i][j]:
+                suspect_counter += 1
+        if suspect_counter == len(matches):
+            suspect = database[i]['name']
+            break
+        else:
+            suspect_counter = 1
+            
+    print(suspect)
 
     return
 
