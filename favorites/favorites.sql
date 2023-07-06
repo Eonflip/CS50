6734947c6c4695ce77588fc31750121ca9cfe5bc
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-07-06 16:44:12 +0000

    Thu, Jul 6, 2023, 10:44 AM -06:00

--- favorites/favorites.sql
+++ favorites/favorites.sql
@@ -2,4 +2,5 @@ UPDATE shows SET title = "Adventure Time" WHERE title LIKE "adven%";
 UPDATE shows SET title = "Arrow" WHERE title LIKE "%row";
 UPDATE shows SET title = "Avatar: The Last Airbender" WHERE title LIKE "avatar%";
 UPDATE shows SET title = "Brooklyn Nine-Nine" WHERE title LIKE "b%9";
-
+UPDATE shows SET title = "Community" WHERE title LIKE "%unity";
+UPDATE shows SET title = "Family Guy" WHERE title LIKE "%guy";
