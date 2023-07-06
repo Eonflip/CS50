20ed8221b4baa1c4c7f0fbc03d46890267e9f211
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-07-06 19:59:36 +0000

    Thu, Jul 6, 2023, 1:59 PM -06:00

--- prophecy/prophecy.py
+++ prophecy/prophecy.py
@@ -1,8 +1,10 @@
 import sys
 import csv
+from cs50 import SQL
 
 def main()
+    db = SQL.("sqlite:///roster.db)
     if sys.argv == 2:
         reader = csv.DictReader(sys.argv[1])
         for row in reader:
-            
+
