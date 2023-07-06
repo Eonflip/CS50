292efc680a238d85117824e23ed750cc290534a8
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-07-06 16:54:21 +0000

    Thu, Jul 6, 2023, 10:54 AM -06:00

--- favorites/favorites.sql
+++ favorites/favorites.sql
@@ -1,8 +1,8 @@
-UPDATE shows SET title = "Adventure Time" WHERE title LIKE "adven%";
-UPDATE shows SET title = "Arrow" WHERE title LIKE "%row";
-UPDATE shows SET title = "Avatar: The Last Airbender" WHERE title LIKE "avatar%";
-UPDATE shows SET title = "Brooklyn Nine-Nine" WHERE title LIKE "b%9";
-UPDATE shows SET title = "Community" WHERE title LIKE "%unity";
-UPDATE shows SET title = "Family Guy" WHERE title LIKE "%guy";
-UPDATE shows SET title = "Friends" WHERE title LIKE "%ends";
-UPDATE shows SET title = "Game of Thrones" WHERE title LIKE "g%" AND "%t";
+UPDATE shows SET title = 'Adventure Time' WHERE title LIKE 'adven%';
+UPDATE shows SET title = 'Arrow' WHERE title LIKE '%row';
+UPDATE shows SET title = 'Avatar: The Last Airbender' WHERE title LIKE 'avatar%';
+UPDATE shows SET title = 'Brooklyn Nine-Nine' WHERE title LIKE 'b%9';
+UPDATE shows SET title = 'Community' WHERE title LIKE '%unity';
+UPDATE shows SET title = 'Family Guy' WHERE title LIKE '%guy';
+UPDATE shows SET title = 'Friends' WHERE title LIKE '%ends';
+UPDATE shows SET title = 'Game of Thrones' WHERE title LIKE 'g%' AND title like '%t';
