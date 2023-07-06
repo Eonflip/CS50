547b701649180b0e975ec49b409155cebd62c390
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-07-06 20:11:39 +0000

    Thu, Jul 6, 2023, 2:11 PM -06:00

--- prophecy/prophecy.py
+++ prophecy/prophecy.py
@@ -3,8 +3,32 @@ import csv
 from cs50 import SQL
 
 def main()
+    # Connect to the SQLite database
     db = SQL.("sqlite:///roster.db)
-    if sys.argv == 2:
-        reader = csv.DictReader(sys.argv[1])
+
+    #Ensure correct usage
+    if sys.argv != 2:
+        print("Usage: python prophecy.py file.csv)
+        return
+
+    with open(sys.argv[1], "r") as f:
+        # Create DictReader object
+        reader = csv.DictReader(f)
+
         for row in reader:
+            # Insert student into database
+            db.execute("INSERT INTO students(id, student_name, house, head) VALUES (?, ?, ?, ?)",
+                       row["id"], row["student_name"], row["house"], row["head"])
+
+
+            # Insert house into database
+            db.execute("INSERT OR IGNORE INTO houses (house, head) VALUES (?, ?)",
+                       row["house"], row["head"])
+
+
+            db.execute("INSERT INTO house_assignment (student_id, house_name) VALUES (?, ?)",
+                        row["id"], row["house"])
+
+if __name__ == "__main__":
+    main()
 
