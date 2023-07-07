4a6f22cad5e3185eb6c4314d7b0bf62bb8c8b5a7
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-07-07 16:53:56 +0000

    Fri, Jul 7, 2023, 10:53 AM -06:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,3 +1,4 @@
-SELECT AVG(ratings.rating) AS avg_rating
+SELECT title, AVG(ratings.rating) AS avg_rating
 FROM movies
 JOIN ratings ON movies.id = ratings.movie_id
+WHERE ratings.rating IS NOT NULL
