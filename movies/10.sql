2aaa501565bc62d1a346aa2170f673454e854996
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-07-08 02:22:51 +0000

    Fri, Jul 7, 2023, 8:22 PM -06:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,4 +1,5 @@
 SELECT DISTINCT people.name
 FROM people
 JOIN directors ON people.id = directors.person_id
-JOIN ratings ON 
+JOIN movies ON directors.movie_id = movies.id
+JOIN ratings ON movies.id = ratings.movie_id
