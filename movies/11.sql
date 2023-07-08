150ab08cd650f83b4f78785f1c69d09b712377e7
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-07-08 02:47:09 +0000

    Fri, Jul 7, 2023, 8:47 PM -06:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,6 +1,7 @@
-SELECT TOP 5 movies.title
+SELECT movies.title
 FROM movies
-JOIN stars ON movies.id = stars.movies_id
+JOIN stars ON movies.id = stars.movie_id
 JOIN people ON people.id = stars.person_id
 WHERE people.name = 'Chadwick Boseman'
-ORDER BY movies.title DESC;
+ORDER BY movies.title DESC
+LIMIT 5;
