76ab02a7621fea61fe498d01caa47ad8d18f8205
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-07-08 13:52:45 +0000

    Sat, Jul 8, 2023, 7:52 AM -06:00

--- movies/12.sql
+++ movies/12.sql
@@ -3,4 +3,4 @@ FROM movies
 JOIN stars AS sd ON movies.id = sd.movie_id
 JOIN people AS jd ON sd.person_id = jd.id AND jd.name = 'Johnny Depp'
 JOIN stars AS sh ON movies.id = sh.movie_id
-JOIN stars AS hc ON sh.person_id = hc.id AND hc.name = 'Helena Bonham Carter';
+JOIN people AS hc ON sh.person_id = hc.id AND hc.name = 'Helena Bonham Carter';
