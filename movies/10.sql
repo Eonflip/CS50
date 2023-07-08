83b5bc8cd33cbeedcc2b38ef9a553da4e8e2c9cf
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-07-08 02:23:51 +0000

    Fri, Jul 7, 2023, 8:23 PM -06:00

--- movies/10.sql
+++ movies/10.sql
@@ -3,3 +3,4 @@ FROM people
 JOIN directors ON people.id = directors.person_id
 JOIN movies ON directors.movie_id = movies.id
 JOIN ratings ON movies.id = ratings.movie_id
+WHERE ratings.rating <= 9.0;
