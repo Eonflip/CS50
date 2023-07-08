3c1edf070afb0ff4cebfadbf25173f7ab23fafc4
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-07-08 14:40:24 +0000

    Sat, Jul 8, 2023, 8:40 AM -06:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,4 +1,7 @@
 SELECT people.name
 FROM people
 JOIN stars AS sd ON people.id = sd.person_id
-JOIN movies AS md ON sd.movie_id = md.id 
+JOIN movies AS md ON sd.movie_id = md.id
+JOIN stars AS sk ON md.id = sk.movie_id
+JOIN people AS kb ON sk.person_id = kb.id
+WHERE kb.name = 'Kevin Bacon' AND kb.birth = 1958 AND people.name <> 'Kevin Bacon';
