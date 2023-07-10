ee5147f54f8008e7297f9df123b20f1dd7b773ac
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-07-10 02:16:55 +0000

    Sun, Jul 9, 2023, 8:16 PM -06:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,4 +1,4 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 SELECT * FROM crime_scene_reports
 SELECT * FROM interviews
-WHERE year = 2021 AND day = 
+WHERE year = 2021 AND day = 28 AND month = 7;
