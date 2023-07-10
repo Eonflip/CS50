352863f7346daa0fa54931c0d96fc6ce94c549c9
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-07-10 02:36:57 +0000

    Sun, Jul 9, 2023, 8:36 PM -06:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,5 +1,12 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
-SELECT * FROM crime_scene_reports
-SELECT * FROM interviews
+-- First I want to get an idea of what the crime_scene_report reads for clues
+SELECT *
+FROM crime_scene_reports
+
+-- After some clues it looks like 3 people had something to say about this theft let's get interviews from that day
+SELECT *
+FROM interviews
 WHERE year = 2021 AND day = 28 AND month = 7;
 
+-- Chasing the first lead I want to find out what ATM transactions happened 
+SELECT * FROM
