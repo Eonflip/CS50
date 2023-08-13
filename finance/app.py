b570830b4c375bae9f536279730e4e308d116d50
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-08-13 14:44:21 +0000

    Sun, Aug 13, 2023, 8:44 AM -06:00

--- finance/app.py
+++ finance/app.py
@@ -104,7 +104,8 @@ def logout():
 @login_required
 def quote():
     """Get stock quote."""
-    """if request.method == "POST":"""
+    if request.method == "GET":
+        return render_template("/quote.html")
 
     return apology("TODO")
 
