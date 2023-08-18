414e9c59d925f2efadd66c71f2a5092effb292ef
Ian Jones <83149984+Eonflip@users.noreply.github.com>
2023-08-18 01:28:45 +0000

    Thu, Aug 17, 2023, 7:28 PM -06:00

--- finance/helpers.py
+++ finance/helpers.py
@@ -70,18 +70,17 @@ def lookup(symbol):
     # Query API
     try:
         response = requests.get(
-            url, cookies={"session": str(uuid.uuid4())}, headers={"User-Agent": "python-requests", "Accept": "*/*"})
+            url,
+            cookies={"session": str(uuid.uuid4())},
+            headers={"User-Agent": "python-requests", "Accept": "*/*"},
+        )
         response.raise_for_status()
 
         # CSV header: Date,Open,High,Low,Close,Adj Close,Volume
         quotes = list(csv.DictReader(response.content.decode("utf-8").splitlines()))
         quotes.reverse()
         price = round(float(quotes[0]["Adj Close"]), 2)
-        return {
-            "name": symbol,
-            "price": price,
-            "symbol": symbol
-        }
+        return {"name": symbol, "price": price, "symbol": symbol}
     except (requests.RequestException, ValueError, KeyError, IndexError):
         return None
 
